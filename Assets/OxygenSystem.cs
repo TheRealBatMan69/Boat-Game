e2ac63fae0d48dd88640327d52db1d4b7a077356
EthanDavies <113374445+Deenhan@users.noreply.github.com>
2024-05-24 14:20:11 +0100

    Oxygen System Init

---
+++ Assets/OxygenSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenSystem : MonoBehaviour
+{
+
+    public Transform player;
+    public float maxOxygen;
+    public float currentOxygen;
+    public float oxygenDecay;
+    public float oxygenRegen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+        currentOxygen = maxOxygen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.y < 20)
+        {
+            currentOxygen -= oxygenDecay * Time.deltaTime;
+        }
+        else if(player.position.y >= 20 && currentOxygen != maxOxygen)
+        {
+            currentOxygen += oxygenRegen * Time.deltaTime;
+        }
+        Debug.Log("Current Oxygen: " + currentOxygen);
+    }
+}
