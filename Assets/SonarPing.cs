095a49a0d0664f351f1d6bc4f8f8a38fb1c740e7
EthanDavies <113374445+Deenhan@users.noreply.github.com>
2024-05-26 13:48:25 +0100

    Progress on Sonar

--- Assets/SonarPing.cs
+++ Assets/SonarPing.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
 
 public class SonarPing : MonoBehaviour
 {
+
+    public Light2D sonarLight;
+    public GameObject sonarCut;
+
+    public float sonarLightInnerRadius;
+    public float sonarLightOuterRadius;
+    public Vector2 sonarCutRadius;
+
+    public float speed;
+    public float intensitySpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sonarLight.intensity = 0f;
+        sonarLight.pointLightInnerRadius = 0f;
+        sonarLight.pointLightOuterRadius = 0f;
+        sonarCut.transform.localScale = new Vector2(0, 0);
+        sonarCut.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            sonarCut.SetActive(true);
+            
+            Debug.Log("Ping!");
+        }
     }
 }
